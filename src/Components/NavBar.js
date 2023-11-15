6054f73ed5916658b44bbd845047d79f0519dd88
Refilwe Gololo <refilwet97@gmail.com>
2023-11-15 13:46:30 +0200

    added media queries

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,20 @@
 
 import './NavBar.css';
 import image1 from '../Assets/logo1.png';
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function NavBar() {
+
+  const [responsive, setResponsive] = useState(false);
+
+  const toggleResponsive = () => {
+    setResponsive(!responsive);
+  };
+
   return (
-    <div className="topnav">
+    // <div className="topnav">
+    <div className={`topnav ${responsive ? 'responsive' : ''}`}>
       <div className="logo">
         <img src={image1} alt="Logo" />
       </div>
@@ -14,6 +22,9 @@ function NavBar() {
       <Link to="/service">Service</Link>
       <Link to="/about">About Us</Link>
       <Link to="/">Home</Link>
+      <Link to="/" className="icon" onClick={toggleResponsive}>
+        &#9776;
+        </Link>
     </div>
   );
 }
